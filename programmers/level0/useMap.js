7a1fdcbea82c4525cf88c72dd5018da6b4060eee
mkhajiit <mkhajiit15@gmail.com>
2025-04-04 19:27:51 +0900

    Map.set 써보기 추가

--- programmers/level0/useMap.js
+++ programmers/level0/useMap.js
@@ -1,7 +1,7 @@
 // Map 사용해보기
 
 const keys = ['name', 'age', 'hobby'];
-const values = ['John', '25', 'Game'];
+const values = ['John', 25, 'Game'];
 
 const map = new Map(
   keys.map((key, index) => {
@@ -11,4 +11,8 @@ const map = new Map(
 
 console.log(map);
 // get메서드로 키를 이용해 값을 읽어온다.
+console.log(map.get('name'));
+// set메서드로 기존 키의 값을 덮어쓴다.
+map.set('name', 'Alice');
+console.log(map);
 console.log(map.get('name'));
